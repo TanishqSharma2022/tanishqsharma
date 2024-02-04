5a77c02c03f8a2f8965815dfb4af374df4cef1d1
Tanishq Sharma <123730739+TanishqSharma2022@users.noreply.github.com>
2024-02-04 17:58:58 +0530

    updated transition.js

--- src/components/transition.js
+++ src/components/transition.js
@@ -1,28 +1,32 @@
-import {motion} from 'framer-motion'
+import React from 'react';
+import { motion } from 'framer-motion';
+
 const transition = (OgComponent) => {
-    return () => (
+    const TransitionComponent = () => (
         <>
             <OgComponent />
             <motion.div            
-            className='slide-in'
-            initial={{scaleY: 0}}
-            animate={{scaleY: 0}}
-            exit={{scaleY: 1}}
-            transition={{duration: 1, ease: [0.22, 1, 0.36, 1]}}
+                className='slide-in'
+                initial={{ scaleY: 0 }}
+                animate={{ scaleY: 0 }}
+                exit={{ scaleY: 1 }}
+                transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
             />
 
             <motion.div            
-            className='slide-out'
-            initial={{scaleY: 1}}
-            animate={{scaleY: 0}}
-            exit={{scaleY: 1}}
-            transition={{duration: 1, ease: [0.22, 1, 0.36, 1]}}
+                className='slide-out'
+                initial={{ scaleY: 1 }}
+                animate={{ scaleY: 0 }}
+                exit={{ scaleY: 1 }}
+                transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
             />
-    </>
-    )
+        </>
+    );
+
+    // Set the display name for the component
     TransitionComponent.displayName = `Transition(${OgComponent.displayName || OgComponent.name || 'Component'})`;
 
     return TransitionComponent;
 };
 
-export default transition
+export default transition;
